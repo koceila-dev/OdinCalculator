e64dcbe517b1d49ac6d8ee5cb5377122f6db9588
Koceila <chergui.koceila@gmail.com>
2023-06-15 15:50:16 +0100

    Ajout de l'égalité avec erreur après Ajout

--- script.js
+++ script.js
@@ -2,53 +2,103 @@ const numbers = document.querySelectorAll(".number-button");
 const operations = document.querySelectorAll(".operator");
 const screenCurrent = document.querySelector(".currentOperation");
 const screenLastOperation = document.querySelector(".lastOperation");
-var val1, Val2;
+const equal = document.querySelector(".equal")
+var val1 = 0;
+var Val2;
 var numero = "";
 var indicateur = 0;
+var index = null;
+let valeurOperation = null;
+let valeurNumero = null;
+var egal = null;
+var indexEgal = false;
 
 numbers.forEach(number => {
   number.addEventListener("click", function () {
-    const value = this.innerText;
+    var value = this.innerText;
+    console.log("value avant la condition if :   " + value)
+
     if (indicateur == 0) {
+
       screenCurrent.innerText = numero + value;
       numero = screenCurrent.innerText;
+      console.log(`numero .............:::: ${numero} `);
       index = 1;
     }
     else {
-      numero = "";
+      screenCurrent.innerText = "";
       indicateur = 0;
-      screenCurrent.innerText = numero + value;
+      screenCurrent.innerText = value;
       numero = screenCurrent.innerText;
+      console.log(`test du else du eventlistener des numeros dans le else:  ** ${numero} **  et value de screenCurrent ${value}`);
+
       index = 1;
     }
   })
 })
-val1 = 0;
+
+
 operations.forEach(operation => {
   operation.addEventListener("click", function () {
+    value = this.innerText;
+    console.log("value  dans operation .............::::  " + value);
+
     if (index == 1) {
       indicateur = 1;
+      if (indexEgal == false) {
+        //function operate(operation, num1, num2)
+        val1 = operate(value, val1, parseFloat(numero));
+        console.log("à l'interieur de la condition de indexEgal")
 
-      val1 = operate(operation.innerText, val1, parseFloat(numero));
-      console.log("ceci est le numéro:  " + numero + " avec la'operation:   " + this.innerText + "         Le résultat est le suivant:  ");
-      console.log("val1    " + val1 + " Numero   " + parseFloat(numero));
-      value = this.innerText;
+      }
 
+      else {
+        console.log("Else de la condition de indexEgal ")
+        val1 = operate(value, egal, parseFloat(numero));
+
+      }
+      if (screenLastOperation.innerText == "") {
+        screenLastOperation.innerText = val1 + value;
+      }
+      else {
+
+      }
+
+      valeurOperation = value;
       screenCurrent.innerText = val1;
-      screenLastOperation.innerText = val1 + value;
+
+
+      console.log(`value:         ${value}    et Val1         ${val1} `);
+      valeurNumero = val1;
+
       val1 = parseFloat(screenCurrent.innerText);
-
       numero = "";
-      index = 0
+      index = 0;
+
     }
-
-
-
-
   })
 })
+equal.addEventListener("click", function () {
+  indicateur = 1;
+  console.log("numero est égal à :   " + numero);
+  screenLastOperation.innerText = valeurNumero + valeurOperation + numero + "=";
+  egal = operate(valeurOperation, parseFloat(valeurNumero), parseFloat(numero));
+  screenCurrent.innerText = egal;
+  indexEgal = true;
+  value = null;
+  index = 0;
+  numero = egal;
+
+  console.log("*********----------*********----------------")
+  console.log(` la valeur d'egal : ${egal} la valeur de numero est : ${numero}`)
+
+
+})
+
 function add(item1, item2) {
+
   return item1 + item2;
+
 
 }
 
